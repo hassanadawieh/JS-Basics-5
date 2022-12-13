2ccba54826afe5f2ff809285d3a9a41ab6724a1b
Hassan Adawie <adawiehhassan@gmail.com>
2022-12-13 16:34:46 +0200

    greetings

---
+++ problem1/problem2.js
@@ -0,0 +1,18 @@
+// var n = prompt("tayp a number :")
+// alert("1"+n)
+
+var input = document.createElement("input");
+var p = document.createElement("p");
+var result = document.createElement("button");
+p.innerHTML = "what is your name ?";
+var p2 = document.createElement("p");
+result.innerHTML = "submit"
+
+document.body.appendChild(p);
+document.body.appendChild(input);
+document.body.appendChild(result);
+document.body.appendChild(p2);
+
+result.onclick = function () {
+  p2.innerHTML = `Hello ${input.value}`;
+};
